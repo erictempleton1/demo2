6a2903378fd2482b3e02f481a71f40412372ae67
etempleton <erictempleton1@gmail.com>
2018-04-08 19:54:46 -0400

    working on adding redux

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+const initialState = {
+    nodes: [],
+};
+
+// https://www.valentinog.com/blog/react-redux-tutorial-beginners/
+
+const rootReducer = (state = initialState, action) => state;
+
+export default rootReducer;
